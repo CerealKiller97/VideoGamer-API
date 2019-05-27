f6a3454acf48c5db2b0c92e62da47d76a782d5f7
CerealKiller97 <bogdanovic.stefan@outlook.com>
2019-05-27 11:10:30 +0200

    Reogranizing fluent validation

---
+++ SharedModels/Fluent/Platform/PlatformFluentValidator.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using FluentValidation;
+
+namespace SharedModels.Fluent.Platform
+{
+	public class PlatformFluentValidator : AbstractValidator<Domain.Platform>
+	{
+		public PlatformFluentValidator()
+		{
+			RuleFor(p => p.Name)
+				.NotEmpty()
+				.IsInEnum();
+		}
+	}
+}
---
+++ SharedModels/Fluent/User/RegisterFluentValidator.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+using FluentValidation;
+using SharedModels.DTO;
+
+namespace SharedModels.Fluent
+{
+	public class RegisterFluentValidator : AbstractValidator<Register>
+	{
+		public RegisterFluentValidator()
+		{
+			RuleFor(u => u.Email)
+				.NotEmpty()
+				.MaximumLength(150)
+				.EmailAddress();
+
+			RuleFor(u => u.FirstName)
+				.NotEmpty()
+				.MinimumLength(3)
+				.MaximumLength(70)
+				.Matches(new Regex("^[A-Z][a-z]+$"));
+
+			RuleFor(u => u.LastName)
+				.NotEmpty()
+				.MinimumLength(3)
+				.MaximumLength(70)
+				.Matches(new Regex("^[A-Z][a-z]+$"));
+
+			RuleFor(u => u.Password)
+				.NotEmpty()
+				.MinimumLength(8);
+		}
+	}
+}
